31830117f6245cd67115fcfc9f41f3414b210611
FrederickCWX <fcweixun@gmail.com>
2022-06-09 10:52:13 +0800

    update

--- src/main/java/vttp2022/workshop3/App.java
+++ src/main/java/vttp2022/workshop3/App.java
@@ -1,13 +1,20 @@
 package vttp2022.workshop3;
 
 /**
- * Hello world!
+ * Main program
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    private static String defaultDb="db";
+    public static void main( String[] args ){
+        //print out first argument db name use to create directory
+        if(args.length > 0)
+            if(args[0] != null){
+                System.out.println( args[0] );
+                App.defaultDb = args[0];
+            }
+        System.out.println(defaultDb);
+        
     }
 }
---
+++ src/main/java/vttp2022/workshop3/Repository.java
@@ -0,0 +1,5 @@
+package vttp2022.workshop3;
+
+public class Repository {
+  
+}
